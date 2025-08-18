3b3f17dfe1b21d007f2dfab464ebf69190e7ca76
Arpit Khandelwal <AKhandelwal1228@gmail.com>
2025-08-18 11:05:14 +0530

    some changes

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,5 @@
 import express from "express";
-import dbConnection  from "./database/dbConnection.js";
+import dbConnection from "./database/dbConnection.js";
 import jobRouter from "./routes/jobRoutes.js";
 import userRouter from "./routes/userRoutes.js";
 import applicationRouter from "./routes/applicationRoutes.js";
@@ -12,10 +12,13 @@ import fileUpload from "express-fileupload";
 const app = express();
 config({ path: "./config/config.env" });
 
+// ✅ Split FRONTEND_URL into multiple origins
+const allowedOrigins = process.env.FRONTEND_URL.split(",");
+
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL],
-    method: ["GET", "POST", "DELETE", "PUT"],
+    origin: allowedOrigins,
+    methods: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
   })
 );
@@ -30,10 +33,13 @@ app.use(
     tempFileDir: "/tmp/",
   })
 );
+
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/job", jobRouter);
 app.use("/api/v1/application", applicationRouter);
+
 dbConnection();
 
 app.use(errorMiddleware);
+
 export default app;
